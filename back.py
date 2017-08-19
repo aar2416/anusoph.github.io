df8e7012360c9a33a41a0d33884ddf661063c204
aar2416 <aar2416@gmail.com>
2017-08-19 18:15:46 +0530

    Delete back.py

--- back.py
+++
@@ -1,8 +0,0 @@
-import cgi
-import HTML
-#calling form values
-form = cgi.FieldStorage()
-
-with open ('fileToWrite.txt','w') as fileOutput:
-    fileOutput.write(form.getValue('name'))
-    fileOutput.write(form.getValue('email'))
